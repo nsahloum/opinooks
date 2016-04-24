39a1a7f6304d1a95c9037817e2507921c4287279
sahloumi <sahloumi.najima@gmail.com>
2016-04-24 18:07:28 +0200

    usershow end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
+        @debatsuser = @user.debats.order('created_at DESC').page(params[:page]).per_page(10)
     end
 end
