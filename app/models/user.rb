257514f2fb0e578b7db68b600e693d9556f97645
arlabet <sahloumi.najima@gmail.com>
2016-04-25 14:05:23 +0000

    validates presence name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :reactions, :dependent => :destroy
   acts_as_voter
   validates_uniqueness_of :name
+  validates_presence_of :name
   def to_param
       "#{id} #{name}".parameterize
   end
